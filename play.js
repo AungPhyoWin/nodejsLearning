b49f757528925032399d813365ec76d29189228c
Aung Phyo Win <aungphyowynn@gmail.com>
2023-09-27 00:54:53 +1000

    Initial comit

---
+++ play.js
@@ -0,0 +1,9 @@
+var aname = 'Max';
+var age = 30;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby){
+    return ('Name is ' + userName  + ', age is ' + userAge + ' User hobby is ' + userHasHobby);  
+}
+
+console.log(summarizeUser(aname,age,hasHobbies));
