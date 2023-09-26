bbe76093e6f3da9565bd8112d4fde1a69dec9316
Aung Phyo Win <aungphyowynn@gmail.com>
2023-09-27 01:25:44 +1000

    change with arrow function

--- play.js
+++ play.js
@@ -1,9 +1,18 @@
-let aname = 'Max';
+const aname = 'Max';
 let age = 30;
 let  hasHobbies = true;
 
 function summarizeUser(userName, userAge, userHasHobby){
     return ('Name is ' + userName  + ', age is ' + userAge + ' User hobby is ' + userHasHobby);  
 }
-
 console.log(summarizeUser(aname,age,hasHobbies)); /// updated with with let
+
+age = 31;
+hasHobbies = false;
+
+const summarizeUser1 = (userName, userAge, userHasHobby) => {
+    return ('Name is ' + userName  + ', age is ' + userAge + ' User hobby is ' + userHasHobby);  
+
+}
+
+console.log(summarizeUser1(aname,age,hasHobbies)); // added with arrow function
